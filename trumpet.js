e13039118759a73128820855320143dcfe2cc76d
azzyblooms <azzyblooms@gmail.com>
2026-02-16 00:06:41 -0500

    minor decorating, added tuning, key, and control options

--- trumpet.js
+++ trumpet.js
@@ -1,23 +1,130 @@
 let trumpetSprite = "tpt";
-const valveKeys = ["p", "o", "i"];
-const partialKeys = ["z", "x", "c", "v", "b", "n", "m"];
+const click1 = new Audio('audio/click1.mp3');
+const click2 = new Audio('audio/click2.mp3');
+const hover = new Audio ('audio/hover.wav');
+const arp = new Audio ('audio/arpeggio.mp3');
+const keyText = document.getElementById('keytext');
+const keyStuff = document.getElementById('keystuff');
+let twelvetet = true;
+let controlRaised = true;
+let root = Math.pow(2, (0/12));
+let playingKey = "Bb";
+let valveKeys = ["p", "o", "i"];
+const partialKeys = ["z", "x", "c", "v", "b", "n", "m", ",", "."];
+const tuningSwitch = document.getElementById("tuningswitch");
+const keyDropdown = document.getElementById("key");
+const controlSwitch = document.getElementById("controlswitch");
 let activeKey = null;
 let previousKey = null;
 const fingers = new Set();
 let currentBb = null;
-
 let position = 1
 const trumpetImage = document.getElementById('trumpet');
+
+controlSwitch.addEventListener('mouseenter', () => {
+    hover.cloneNode(true).play();
+})
+keyStuff.addEventListener('mouseenter', () => {
+    hover.cloneNode(true).play();
+})
+tuningSwitch.addEventListener('mouseenter', () => {
+    hover.cloneNode(true).play();
+})
+controlSwitch.addEventListener('mouseleave', () => {
+    hover.cloneNode(true).play();
+})
+keyStuff.addEventListener('mouseleave', () => {
+    hover.cloneNode(true).play();
+})
+tuningSwitch.addEventListener('mouseleave', () => {
+    hover.cloneNode(true).play();
+})
+controlSwitch.addEventListener('mousedown', () => {
+    click1.cloneNode(true).play();
+})
+controlSwitch.addEventListener('mouseup', () => {
+    click2.cloneNode(true).play();
+})
+tuningSwitch.addEventListener('mousedown', () => {
+    click1.cloneNode(true).play();
+})
+tuningSwitch.addEventListener('mouseup', () => {
+    click2.cloneNode(true).play();
+    twelvetet = !twelvetet;
+})
+keyDropdown.addEventListener('change', () => {
+    document.activeElement.blur();
+    arp.cloneNode(true).play();
+    playingKey = keyDropdown.value;
+    if(keyDropdown.value == "Bb") {
+        root = Math.pow(2, (0/12));
+    }
+    if(keyDropdown.value == "B") {
+        root = Math.pow(2, (1/12));
+    }
+    if(keyDropdown.value == "C") {
+        root = Math.pow(2, (2/12));
+    }
+    if(keyDropdown.value == "Db") {
+        root = Math.pow(2, (3/12));
+    }
+    if(keyDropdown.value == "D") {
+        root = Math.pow(2, (4/12));
+    }
+    if(keyDropdown.value == "Eb") {
+        root = Math.pow(2, (5/12));
+    }
+    if(keyDropdown.value == "E") {
+        root = Math.pow(2, (6/12));
+    }
+    if(keyDropdown.value == "F") {
+        root = Math.pow(2, (7/12));
+    }
+    if(keyDropdown.value == "Gb") {
+        root = Math.pow(2, (-4/12));
+    }
+    if(keyDropdown.value == "G") {
+        root = Math.pow(2, (-3/12));
+    }
+    if(keyDropdown.value == "Ab") {
+        root = Math.pow(2, (-2/12));
+    }
+    if(keyDropdown.value == "A") {
+        root = Math.pow(2, (-1/12));
+    }
+    console.log(playingKey);
+})
+controlSwitch.addEventListener('click', () => {
+    controlRaised = !controlRaised;
+    console.log(controlRaised)
+})
+
 function getPlaybackRate(key, position) {
-    baseFactors = {
-        z: 1 * 2,
-        x: 1.498 * 2,
-        c: 2 * 2,
-        v: 2.52 * 2,
-        b: 2.998 * 2,
-        n: 3.567 * 2,
-        m: 4 * 2,
-        Comma: 5
+    if(twelvetet == true) {
+        baseFactors = {
+        z: Math.pow(2, (0/12)) * 2 * root,
+        x: Math.pow(2, (7/12)) * 2 * root,
+        c: Math.pow(2, (12/12)) * 2 * root,
+        v: Math.pow(2, (16/12)) * 2 * root,
+        b: Math.pow(2, (19/12)) * 2 * root,
+        n: Math.pow(2, (22/12)) * 2 * root,
+        m: Math.pow(2, (24/12)) * 2 * root,
+        ",": Math.pow(2, (26/12)) * 2 * root,
+        ".": Math.pow(2, (28/12)) * 2 * root
+    }
+    }
+    else {
+        baseFactors = {
+        z: 1 * 2 * root,
+        x: 1.5 * 2 * root,
+        c: 2 * 2 * root,
+        v: 2.5 * 2 * root,
+        b: 3 * 2 * root,
+        n: 3.5 * 2 * root,
+        m: 4 * 2 * root,
+        ",": 4.5 * 2 * root,
+        ".": 5 * 2 * root
+    }
     }
     const factor = Math.pow(Math.pow(2, (-0.5)), (position - 1) / 6);
     return baseFactors[key] * factor;
@@ -34,7 +141,8 @@ function playBb(rate) {
 }
 function checkFingers() {
     const fingering = [...fingers].sort().join("");
-    switch (fingering) {
+    if(controlRaised == true) {
+        switch (fingering) {
         case "p":
             trumpetImage.src = "images/tpt1.png";
             position = 3;
@@ -67,6 +175,43 @@ function checkFingers() {
             trumpetImage.src = "images/tpt.png"
             position = 1;
         getPlaybackRate();
+    }
+    }
+    else {
+        switch (fingering) {
+        case "i":
+            trumpetImage.src = "images/tpt1.png";
+            position = 3;
+            break;
+        case "o":
+            trumpetImage.src = "images/tpt2.png";
+            position = 2;
+            break;
+        case "p":
+            trumpetImage.src = "images/tpt3.png";
+            position = 4;
+            break;
+        case "io":
+            trumpetImage.src = "images/tpt12.png";
+            position = 4;
+            break;
+        case "ip":
+            trumpetImage.src = "images/tpt13.png";
+            position = 6;
+            break;
+        case "op":
+            trumpetImage.src = "images/tpt23.png"
+            position = 5;
+            break;
+        case "iop":
+            trumpetImage.src = "images/tpt123.png"
+            position = 7;
+            break;
+        default:
+            trumpetImage.src = "images/tpt.png"
+            position = 1;
+        getPlaybackRate();
+    }
     }
 }
 
